3b95bcc76c0cc5b802aed5aba083c132834ef529
utk9 <utkarshlamba.2@gmail.com>
2016-06-15 20:46:53 -0400

    Added battle route

---
+++ githubbattle/app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      isLoading: true,
+      playersInfo: [],
+    }
+  },
+
+  render: function () {
+    return (
+      <ConfirmBattle />
+    )
+  }
+});
+
+module.exports = ConfirmBattleContainer;
