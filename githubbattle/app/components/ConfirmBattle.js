3e9ac52dc23ae53cf9b996d61a03fa7ccb2b2a07
utk9 <utkarshlamba.2@gmail.com>
2016-06-15 23:20:01 -0400

    Added data fetching from github api

--- githubbattle/app/components/ConfirmBattle.js
+++ githubbattle/app/components/ConfirmBattle.js
@@ -1,9 +1,13 @@
 var React = require('react');
 
+function puke (obj) {
+  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
-	return (
-		<div>Confirm Battle</div>
-	);
+	return props.isLoading === true
+    ? <p>LOADING</p>
+    : <div>CONFIRM BATTLE: {puke(props.playersInfo)}</div>
 }
 
 module.exports = ConfirmBattle;
--- githubbattle/app/containers/ConfirmBattleContainer.js
+++ githubbattle/app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -11,10 +12,20 @@ var ConfirmBattleContainer = React.createClass({
       playersInfo: [],
     }
   },
+  componentDidMount: function() {
+    var query = this.props.location.query;
+    var that = this;
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo]).then(function(players){
+      that.setState({
+        isLoading: false,
+        playersInfo: [ players[0], players[1] ]
+      })
+    });
+  },
 
   render: function () {
     return (
-      <ConfirmBattle />
+      <ConfirmBattle isLoading={this.state.isLoading} playersInfo={this.state.playersInfo}/>
     )
   }
 });
